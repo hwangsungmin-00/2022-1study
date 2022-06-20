f0462aa8dfe3f972a4f76e08917a330a67484a87
hwangsungmin <hwangsungmin_00@naver.com>
2022-06-21 01:33:19 +0900

    카톡 유사 알림 만들기 진동도 포함

---
+++ mobileapp/MyApplication10/ch10_notification/src/main/java/com/example/ch10_notification/ReplyReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.ch10_notification
+
+import android.app.NotificationManager
+import android.app.RemoteInput
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class ReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        //TODO("ReplyReceiver.onReceive() is not implemented")
+
+        val replyTxt=RemoteInput.getResultsFromIntent(intent)?.getCharSequence("key_text_reply")
+        Log.d("mobileApp", "$replyTxt")
+        val manager = context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        manager.cancel(11)
+    }
+}
